e753736a39bb414296d2d1b6bc843ce979e17805
Promise Mphoola <promisemphoola2@gmail.com>
2022-08-15 19:45:14 +0200

    make it better

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,15 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Product::factory(50)->create();
+        \App\Models\Product::factory(10)->create();
         \App\Models\User::factory(10)->create();
         \App\Models\Expense::factory(10)->create();
         \App\Models\Sale::factory(10)->create();
         \App\Models\SaleItem::factory(100)->create();
 
         \App\Models\User::create([
-            'name' => 'Admin',
+            'name' => 'Promise Mphoola',
             'email' => 'promisemphoola2@gmail.com',
+            'password' => Hash::make('1234567890')]);
+        \App\Models\User::create([
+            'name' => 'Waleke Khumalo',
+            'email' => 'walekekhumalo@gmail.com',
             'password' => Hash::make('1234567890')]);
     }
 }
